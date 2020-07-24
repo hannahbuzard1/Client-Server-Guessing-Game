33516a5142a271839d2e4cec782a837916a6dd85
buzardh <buzardh@wwu.edu>
2020-07-24 01:56:21 +0000

    Upload New File

---
+++ demo_client.c
@@ -0,0 +1,111 @@
+/* demo_client.c - code for example client program that uses TCP */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+/*------------------------------------------------------------------------
+* Program: demo_client
+*
+* Purpose: allocate a socket, connect to a server
+*
+* Syntax: ./demo_client server_address server_port
+*
+* server_address - name of a computer on which server is executing
+* server_port    - protocol port number server is using
+*
+*-------------------------------------------------------------------------
+*/
+int main( int argc, char **argv) {
+	struct hostent *ptrh; /* pointer to a host table entry */
+	struct protoent *ptrp; /* pointer to a protocol table entry */
+	struct sockaddr_in sad; /* structure to hold an IP address */
+	int sd; /* socket descriptor */
+	int port; /* protocol port number */
+	char *host; /* pointer to host name */
+	int n; /* number of characters read */
+
+	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
+	sad.sin_family = AF_INET; /* set family to Internet */
+
+	if( argc != 3 ) {
+		fprintf(stderr,"Error: Wrong number of arguments\n");
+		fprintf(stderr,"usage:\n");
+		fprintf(stderr,"./client server_address server_port\n");
+		exit(EXIT_FAILURE);
+	}
+
+	port = atoi(argv[2]); /* convert to binary */
+	if (port > 0) /* test for legal value */
+		sad.sin_port = htons((u_short)port);
+	else {
+		fprintf(stderr,"Error: bad port number %s\n",argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	host = argv[1]; /* if host argument specified */
+
+	/* Convert host name to equivalent IP address and copy to sad. */
+	ptrh = gethostbyname(host);
+	if ( ptrh == NULL ) {
+		fprintf(stderr,"Error: Invalid host: %s\n", host);
+		exit(EXIT_FAILURE);
+	}
+
+	memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+
+	/* Map TCP transport protocol name to protocol number. */
+	if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Create a socket. */
+	sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+	if (sd < 0) {
+		fprintf(stderr, "Error: Socket creation failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Connect the socket to the specified server. */
+	if (connect(sd, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
+		fprintf(stderr,"connect failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "Successfully connected to server.\n");
+
+	bool correct = false;
+	int userguess = 0;
+	int n =0;
+	//keep asking for a guess until told to exit or the guess is correct
+	while (correct != true) {
+		//ask user for guess and send guessed number to server
+		char str2[12];
+		scanf("%d", &userguess);
+		sprintf(str2, "%d", userguess); //convert guess to a string to be sent to server
+		send(sd, str2, 12, 0);
+		//process the server's reply to the guess
+		char buf[1000];  // buffer for data from the server
+		n = recv(sd2, buf, sizeof(buf), 0);
+		//convert reply to integer
+		int reply = atoi(buf);
+		if(reply == 1) {
+			printf("Guess is too high, try again\n", %s);
+		} else if (reply == -1) {
+			printf("Guess is too low, try again\n", %s);
+		} else if (reply == 0) {
+			printf("You have guessed correctly and won the game!!\n", %s);
+			correct = true;
+		}
+	}
+	close(sd);
+	exit(EXIT_SUCCESS);
+}
